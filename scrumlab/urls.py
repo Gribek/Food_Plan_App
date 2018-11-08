91da53b8a460a1e3839ee3d9275449eb2cd61dd2
Marcin Baryłka <marcin.barylka@coderslab.pl>
2018-11-08 13:49:52 +0100

    Added frontend HTMLs

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from jedzonko.views import IndexView, TestView
+from jedzonko.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
-    path('test/', TestView.as_view()),
 ]
