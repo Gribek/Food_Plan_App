32a0031fb03617b5d50a6c4a99d4d8370ce0be8f
Milan Nowak <milan.nowak90@gmail.com>
2018-11-27 11:58:49 +0100

    dodałem funkcjonalność zliczania ilości planów i przepisów

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -7,26 +7,8 @@ from django.shortcuts import render
 from django.views import View
 
 
-class IndexView(View):
-
-    def get(self, request):
-        model_lenght = Recipe.objects.count()
-        result = [i for i in range(1, model_lenght)]
-        shuffle(result)
-
-        recipe = Recipe.objects.get(pk=result[0])
-        recipe2 = Recipe.objects.get(pk=result[1])
-        recipe3 = Recipe.objects.get(pk=result[2])
-
-        ctx = {"recipe": recipe,
-               "recipe2": recipe2,
-               "recipe3": recipe3,
-               }
-
-        return render(request, "index.html", ctx)
-
-
 def add_test_data_to_database(request):
+    """
     Recipe.objects.create(name="Gulasz", ingredients="mięso, papryka, i kilka innych",
                           description="shdbdicnsidyfgnciudygfcyugnifyseivfumxsyerifsrxmirybg",
                           preparation_time=30)
@@ -45,30 +27,68 @@ def add_test_data_to_database(request):
     Recipe.objects.create(name="zapiekanka", ingredients="asfag, sdgvdh, rgdrtgdt, dtrcgtfcgbt",
                           description="shdbdicnsidyfgnciudygfcyugnifyseivfumxsyerifsrxmirybg",
                           preparation_time=30)
-        
+
+    Plan.objects.create(name="jak u mamy", description="pozywny i smaczny")
+    Plan.objects.create(name="jak u babci", description="mięsko zjedz ziemniaczki zostaw")
+    Plan.objects.create(name="jak u cioci", description="lepiej niż na imieninach")
+    Plan.objects.create(name="jak u dziadka", description="dawaj wnuczku na drugą")
+
+    Recipeplan.objects.create(meal_name="śniadanie", order=1, day_name=1, plan_id=1, recipe_id=1)
+    Recipeplan.objects.create(meal_name="obiad", order=2, day_name=1, plan_id=1, recipe_id=3)
+    Recipeplan.objects.create(meal_name="kolacja", order=3, day_name=1, plan_id=1, recipe_id=4)
+    Recipeplan.objects.create(meal_name="śniadanie", order=1, day_name=3, plan_id=2, recipe_id=7)
+    Recipeplan.objects.create(meal_name="obiad", order=2, day_name=3, plan_id=2, recipe_id=5)
+    Recipeplan.objects.create(meal_name="kolacja", order=3, day_name=3, plan_id=2, recipe_id=2)
+    Recipeplan.objects.create(meal_name="śniadanie", order=1, day_name=3, plan_id=2, recipe_id=6)
+    Recipeplan.objects.create(meal_name="obiad", order=2, day_name=3, plan_id=3, recipe_id=1)
+    Recipeplan.objects.create(meal_name="kolacja", order=3, day_name=3, plan_id=3, recipe_id=2)
+"""
+
 
 class LandingPage(View):
 
     def get(self, request):
-        return render(request, "index.html")
+        model_lenght = Recipe.objects.count()
+        result = [i for i in range(1, model_lenght)]
+        shuffle(result)
+
+        recipe = Recipe.objects.get(pk=result[0])
+        recipe2 = Recipe.objects.get(pk=result[1])
+        recipe3 = Recipe.objects.get(pk=result[2])
+
+        ctx = {"recipe": recipe,
+               "recipe2": recipe2,
+               "recipe3": recipe3,
+               }
+
+        return render(request, "index.html", ctx)
+
 
 class Recipe_List(View):
 
     def get(self, request):
         return render(request, "recipes.html")
 
+
 class ContactView(View):
 
     def get(self, request):
         return render(request, "index.html")
+
 
 class AboutView(View):
 
     def get(self, request):
         return render(request, "index.html")
 
+
 class MainPage(View):
 
     def get(self, request):
-        return render(request, "dashboard.html")
-
+        plans_amount = Plan.objects.count()
+        recipes_amount = Recipe.objects.count()
+        ctx = {
+            'plans': plans_amount,
+            'recipes': recipes_amount,
+        }
+        return render(request, "dashboard.html", ctx)
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -17,16 +17,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 from jedzonko.views import *
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$', IndexView.as_view()),
     path('add', add_test_data_to_database),
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', IndexView.as_view()),
     re_path(r'^$', LandingPage.as_view()),
     re_path('^main$', MainPage.as_view()),
     # re_path(r'recipe/(?P<id>\d+)', ),
